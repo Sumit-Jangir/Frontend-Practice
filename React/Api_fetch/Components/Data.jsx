4f29eb864b58b10d9ee9c310a53536b40c7784c2
Sumit Jangir <jangirsumit1011@gmail.com>
2024-09-29 22:44:25 +0530

    Add Search Input to Search product

--- React/Api_fetch/Components/Data.jsx
+++ React/Api_fetch/Components/Data.jsx
@@ -6,24 +6,51 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 function Data() {
   const [fetch, setFetch] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [Search, setSearch] = useState("");
+  const [Sort, setSort] = useState("");
 
   async function fetchData() {
     const res = await axios.get("https://fakestoreapi.com/products");
     setFetch(res.data);
     setLoading(false);
+    console.log(res.data);
   }
 
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(fetch);
+
+  const Filter = fetch.filter((item) => {
+    return (
+      item.title?.toLowerCase().includes(Search?.toLowerCase() || '') ||
+      item.description?.toLowerCase().includes(Search?.toLowerCase() || '') ||
+      item.category?.toLowerCase().includes(Search?.toLowerCase() || '')
+    );
+  });
+
+  
+
+  const Sorttest = () => {
+    if (Sort == "a") {
+      return [...Filter].sort((a, b) => a.price - b.price);
+    }
+    if (Sort == "b") {
+      return [...Filter].sort((a, b) => b.price - a.price);
+    }
+    return Filter;
+  };
 
   return (
     <div className="main">
+      <input type="search" onInput={(e) => setSearch(e.target.value)} />
+      <button onClick={() => setSort("a")}>Low to high</button>
+      <button onClick={() => setSort("b")}>High to low</button>
       <div className="row">
-        {loading ? (<h1>Loading...</h1>) : (
-        fetch &&
-          fetch.map((item, i) => (
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          Sorttest() &&
+          Sorttest().map((item, i) => (
             <div className="card" key={i} style={{ width: "20rem" }}>
               <LazyLoadImage
                 src={item.image}
@@ -41,8 +68,7 @@ function Data() {
                 <h3>$ {item.price}</h3>
                 <h6>Rating: {item.rating.rate}</h6>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  {item.description}
                 </p>
                 {/* <a href="#" className="btn btn-primary">
                 Go somewhere
@@ -50,7 +76,7 @@ function Data() {
               </div>
             </div>
           ))
-           )}
+        )}
       </div>
     </div>
   );
