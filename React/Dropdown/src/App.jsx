36c356ea46e00c3a412f6f275713715fb1cda41f
Sumit Jangir <jangirsumit1011@gmail.com>
2024-10-05 22:59:59 +0530

    dropdown project

---
+++ React/Dropdown/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Dropdown from "./Dropdown";
+import ApiFetching from "./ApiFetching";
+
+const App = () => {
+  return (
+    <>
+    <div style={{width:"100%", height:"10vh", margin:"30px 400px" }}>
+      <Dropdown />
+
+    </div>
+      {/* <ApiFetching /> */}
+    </>
+  );
+};
+
+export default App;
